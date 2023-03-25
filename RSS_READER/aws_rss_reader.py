e9bab8766f8d9e52b4c73642d30cdddb36368797
Hobomechanistic <dejagerrob@gmail.com>
2023-03-25 12:13:51 +0000

    Add html generation to python script

--- RSS_READER/aws_rss_reader.py
+++ RSS_READER/aws_rss_reader.py
@@ -1,9 +1,8 @@
 import feedparser
 import json
-import asyncio
+import os
 from datetime import datetime
 from time import mktime
-
 
 # Security Bulletin RSS feed = https://aws.amazon.com/security/security-bulletins/feed/
 # What’s New RSS feed = https://aws.amazon.com/about-aws/whats-new/recent/feed/
@@ -25,7 +24,7 @@ def normalize_time(time):
 
     return tm
 
-def isNewer(date_one, date_two):
+def is_newer(date_one, date_two):
     dt_obj1 = datetime.strptime(date_one, "%d-%m-%Y")
     dt_obj2 = datetime.strptime(date_two, "%d-%m-%Y")
     if dt_obj2 > dt_obj1:
@@ -34,27 +33,186 @@ def isNewer(date_one, date_two):
     else:
         return False
 
+def build_html(data):
+    html_top = """
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>What's Up Doc?</title>
+  </head>
+
+  <body>
+    <h2 class="header-style"><span style="color: #FF9900">W</span>hat's <span style="color: #FF9900">U</span>p <span style="color: #FF9900">D</span>oc?</h2>
+    <p style="text-align: center; font-family: arial;">AWS RSS feed aggregator, click to sort and have a great day!</p>
+
+    <style>
+        .header-style {
+            font-size: 5em;
+            font-family: arial;
+            text-align: center;
+            height: 0.5em;
+        }
+        .styled-table {
+            border-collapse: collapse;
+            margin: 25px 0;
+            font-size: 0.9em;
+            font-family: sans-serif;
+            min-width: 400px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .styled-table thead tr {
+            background-color: #009879;
+            color: #ffffff;
+            text-align: left;
+        }
+        .styled-table th {
+            cursor: pointer;
+            height: 70px;
+        }
+        .styled-table td {
+            padding: 12px 15px;
+        }
+        .styled-table tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+        .styled-table tbody tr:nth-of-type(even) {
+            background-color: #f3f3f3;
+        }
+        .styled-table tbody tr:last-of-type {
+            border-bottom: 2px solid #FF9900;
+        }
+        .styled-table tbody tr.active-row {
+            font-weight: bold;
+            color: #009879;
+        }
+        th:hover {background-color: #FF9900;}
+    </style>
+
+    <script>
+    """
+    html_json = "var myJSON = ['" + json.dumps(data) + "']"
+    html_bot = """
+    document.write('<table class="styled-table", id="myTable2">');
+    document.write('<tbody>');
+    document.write("<tr>");
+    document.write('<th onclick="sortTable(0)"> Service </th>');
+    document.write('<th onclick="sortTable(1)"> Tag </th>');
+    document.write('<th onclick="sortTable(2)"> Date </th>');
+    document.write('<th onclick="sortTable(3)"> Time </th>');
+    document.write('<th onclick="sortTable(4)"> Summary </th>');
+    document.write('<th onclick="sortTable(5)"> Source </th>');
+    document.write("</tr>");
+
+    const myObj = JSON.parse(myJSON[0]);
+    var test = "blah"
+    for (key in myObj) {
+        test = myObj[key];
+
+        for (x in test) {
+            document.write("<tr>");
+            document.write('<td>' + JSON.stringify(test[x]['Name']).replace(/^"(.*)"$/,'$1') + '</td>');
+            document.write('<td>' + JSON.stringify(test[x]['Tag']).replace(/"/g,'') + '</td>');
+            document.write('<td>' + JSON.stringify(test[x]['Date']).replace(/^"(.*)"$/,'$1') + '</td>');
+            document.write('<td>' + JSON.stringify(test[x]['Time']).replace(/^"(.*)"$/,'$1') + '</td>');
+            document.write('<td>' + JSON.stringify(test[x]['Summary']).replace(/^"(.*)"$/,'$1') + '</td>');
+            document.write('<td>' + JSON.stringify(test[x]['Link']).replace(/^"(.*)"$/,'$1') + '</td>');
+            document.write("</tr>");
+        }
+    }
+    document.write('</tbody>');
+    document.write("</table>");
+
+    function sortTable(n) {
+      var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+      table = document.getElementById("myTable2");
+      switching = true;
+      // Set the sorting direction to ascending:
+      dir = "asc";
+      /* Make a loop that will continue until
+      no switching has been done: */
+      while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+          // Start by saying there should be no switching:
+          shouldSwitch = false;
+          /* Get the two elements you want to compare,
+          one from current row and one from the next: */
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          /* Check if the two rows should switch place,
+          based on the direction, asc or desc: */
+          if (dir == "asc") {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else if (dir == "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          /* If a switch has been marked, make the switch
+          and mark that a switch has been done: */
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          // Each time a switch is done, increase this count by 1:
+          switchcount ++;
+        } else {
+          /* If no switching has been done AND the direction is "asc",
+          set the direction to "desc" and run the while loop again. */
+          if (switchcount == 0 && dir == "asc") {
+            dir = "desc";
+            switching = true;
+          }
+        }
+      }
+    }
+    </script>
+  </body>
+</html>
+    """
+    return(html_top + html_json + html_bot)
+
 def update(url):
-    # use get to get data not direct key access
+    #Todo use get to get data not direct key access
     feed = feedparser.parse(url)
     for entry in feed.entries:
         for x in services_list:
             for y in services_list[x]:
                 name = y.get("Name")
                 if name in entry.title:
-                    if isNewer(y.get("Date"), normalize_date(entry.published_parsed)):
+                    if is_newer(y.get("Date"), normalize_date(entry.published_parsed)):
                         y["Date"] = normalize_date(entry.published_parsed)
                         y["Time"] = normalize_time(entry.published_parsed)
                         y["Summary"] = entry.title
-                        print(y["Summary"])
-                        print(y["Time"])
-                        print(y["Date"])
+                        y["Link"] = entry.link
 
+def write_output(services_list):
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    f = open("output/index.html", "w")
+    f.write(build_html(services_list))
+    f.close()
+
+    with open('services.json', "w") as json_file:
+        json.dump(services_list, json_file)
 
 def main():
 
     rss_urls = ["https://aws.amazon.com/blogs/aws/feed/","https://aws.amazon.com/security/security-bulletins/feed/","https://aws.amazon.com/about-aws/whats-new/recent/feed/"]
-    #rss_urls = ["https://aws.amazon.com/security/security-bulletins/feed/"]
     for url in rss_urls:
         update(url)
 
@@ -65,8 +223,4 @@ if __name__ == "__main__":
         services_list = json.load(json_file)
 
     main()
-
-    with open('services.json', "w") as json_file:
-        json.dump(services_list, json_file)
-
-
+    write_output(services_list)
