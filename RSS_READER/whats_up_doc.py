f1f183a887a10a3a7d737967eefb0b8c5e1dad4d
Hobomechanistic <dejagerrob@gmail.com>
2023-03-28 13:43:14 +0100

    Add some search functionality

--- RSS_READER/whats_up_doc.py
+++ RSS_READER/whats_up_doc.py
@@ -97,6 +97,7 @@ def build_html(data):
     """
     html_json = "var myJSON = ['" + json.dumps(data) + "']"
     html_bot = """
+    document.write('<input type="text" id="myInput" onkeyup="searchFunc()" placeholder="Search Summary for text..." title="Type in a string">');
     document.write('<table class="styled-table", id="myTable2">');
     document.write('<tbody>');
     document.write("<tr>");
@@ -126,6 +127,25 @@ def build_html(data):
     }
     document.write('</tbody>');
     document.write("</table>");
+
+    function searchFunc() {
+      var input, filter, table, tr, td, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable2");
+      tr = table.getElementsByTagName("tr");
+      for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[4];
+          if (td) {
+              txtValue = td.textContent || td.innerText;
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                  tr[i].style.display = "";
+              } else {
+                  tr[i].style.display = "none";
+              }
+          }
+      }
+    }
 
     function sortTable(n) {
       var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
